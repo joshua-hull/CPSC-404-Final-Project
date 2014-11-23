9ce11c450b42502e51fb98af8aaaffdc014e3006
Joshua Hull <joshua@joshuahull.net>
2014-11-22 21:37:51 -0500

    Interactive sliders.

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 jQuery(document).ready(function(){
-  var redSlider;
-  jQuery( "#slider" ).slider(
+  var redSliderValue;
+  var greenSliderValue;
+  var blueSliderValue;
+  jQuery("#redSlider").slider(
     {
       max: 255,
       min: 0,
-      change: function( event, ui ) {
-        redSlider = ui.value;
-        console.log("slider value changed: " . redSlider);
-        jQuery("#sliderLabel").text(redSlider);
+      slide: function(event, ui) {
+        redSliderValue = ui.value;
+        jQuery("#redSliderLabel").text("Value: " + redSliderValue);
+      },
+      create: function(event, ui) {
+        ui.value = 0;
       }
-    });
-  });
+    }).css("background","#ff0000");
+
+    jQuery("#greenSlider").slider(
+      {
+        max: 255,
+        min: 0,
+        slide: function(event, ui) {
+          greenSliderValue = ui.value;
+          jQuery("#greenSliderLabel").text("Value: " + greenSliderValue);
+        },
+        create: function(event, ui) {
+          ui.value = 0;
+        }
+      }).css("background","#00ff00");
+
+      jQuery("#blueSlider").slider(
+        {
+          max: 255,
+          min: 0,
+          slide: function( event, ui ) {
+            blueSliderValue = ui.value;
+            jQuery("#blueSliderLabel").text("Value: " + blueSliderValue);
+          },
+          create: function(event, ui) {
+            ui.value = 0;
+          }
+        }).css("background","#0000ff");
+      });
